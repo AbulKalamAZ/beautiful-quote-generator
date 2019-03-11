ba1d512c107fcd57771bd6f411f8fca3be8178b7
ak_azad <ak_azad@solsersoft.com>
2019-03-12 02:57:06 +0600

    app architechture re configured

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,19 @@
 <template>
   <div class="container">
-    <button @click="con('appQuote')">Quote</button>
-    <button @click="con('appAuthor')">Author</button>
-    <button @click="con('appNew')">New</button>
-    <hr>
-    <component v-bind:is="selectedComponent"></component>
+    
   </div>
 </template>
 
 <script>
-import Quote from "./components/Quote";
-import Author from "./components/Author";
-import New from "./components/New";
 
 export default {
   components: {
-    app: "Quote",
-    appAuthor: "Author",
-    appNew: "New"
   },
   data() {
     return {
-      selectedComponent: "appQuote"
     };
   },
   methods: {
-    con(data) {
-      this.selectedComponent = data;
-    }
   }
 };
 </script>
